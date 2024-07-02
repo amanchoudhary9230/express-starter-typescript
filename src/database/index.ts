bdd8a7ec80661ed508f5dda10ebe321764054091
amanchoudhary9230 <amanjaat7668@gmail.com>
2024-07-02 16:23:59 +0530

    replace tsconfig.josn with ours

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,9 +1,9 @@
-import * as mongoose from 'mongoose'
+import * as mongoose from "mongoose";
 
 export default (uri: string) => {
   try {
-    mongoose.connect(uri)
-  } catch (e) {
-    console.log('Error connecting to mongo db', e.message)
+    mongoose.connect(uri);
+  } catch (e: any) {
+    console.log("Error connecting to mongo db", e.message);
   }
-}
+};
